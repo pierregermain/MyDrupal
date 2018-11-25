80610a51816d47fa11588930602e5a2f501dc401
Pierre Germain <pierre2008@gmail.com>
2018-11-25 21:44:37 +0100

    commit from arch

--- d8/hello_world/src/HelloWorldSalutation.php
+++ d8/hello_world/src/HelloWorldSalutation.php
@@ -1,7 +1,22 @@
 <?php
 
-namespace Drupal\ hello_world; use Drupal\ Core\ StringTranslation\ StringTranslationTrait; /** * Prepares the salutation to the world. */ class HelloWorldSalutation { use StringTranslationTrait;
+namespace Drupal\hello_world;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
-/** * Returns the salutation */ public function getSalutation() { $ time = new \DateTime(); if (( int) $ time-> format(' G') > = 06 && (int) $ time-> format(' G') < 12) { return $ this-> t(' Good morning world'); } if (( int) $ time-> format(' G') > = 12 && (int) $ time-> format(' G') < 18) { return $ this-> t(' Good afternoon world'); } if (( int) $ time-> format(' G') > = 18) { return $ this-> t(' Good evening world'); } } }
+/** 
+* Prepares the salutation to the world. 
+*/ 
+class HelloWorldSalutation { 
+use StringTranslationTrait;
+
+/**
+ * Returns the salutation 
+*/ 
+public function getSalutation() { 
+	$time = new \DateTime();
+		if (( int) $time-> format(' G') > = 06 && (int) $ time-> format(' G') < 12) { return $ this-> t(' Good morning world');
+		} if (( int) $ time-> format(' G') > = 12 && (int) $ time-> format(' G') < 18) { return $ this-> t(' Good afternoon world');
+		} if (( int) $ time-> format(' G') > = 18) { return $ this-> t(' Good evening world');
+		} } }
 
 
