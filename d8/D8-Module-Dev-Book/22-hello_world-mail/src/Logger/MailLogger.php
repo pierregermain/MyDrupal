4af099a18f27953351d7e361c06a6b601b961c62
Pierre Bertram <pierre.bertram@isoinmail.com>
2019-03-27 15:36:41 +0100

    Remove unused code

--- d8/D8-Module-Dev-Book/22-hello_world-mail/src/Logger/MailLogger.php
+++ d8/D8-Module-Dev-Book/22-hello_world-mail/src/Logger/MailLogger.php
@@ -45,13 +45,12 @@ class MailLogger implements LoggerInterface {
     if ($level !== RfcLogLevel::ERROR) {
       return;
     }
-    /** @var AccountProxyInterface $account */
-    $account = $context['user'];
+
     $to = $this->configFactory->get('system.site')->get('mail'); // Default Site Email at /admin/config/system/site-information
     $langcode = $this->configFactory->get('system.site')->get('langcode');
     $variables = $this->parser->parseMessagePlaceholders($message, $context);
     $markup = new FormattableMarkup($message, $variables);
-    \Drupal::service('plugin.manager.mail')->mail('hello_world', 'hello_world_log', $to, $langcode, ['message' => $markup, 'user' => $account]);
+    \Drupal::service('plugin.manager.mail')->mail('hello_world', 'hello_world_log', $to, $langcode, ['message' => $markup]);
   }
 
 
